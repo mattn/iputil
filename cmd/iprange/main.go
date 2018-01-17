64bb59c7bd4c4117e06d20b3da36d07034f7f9fb
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 19:58:22 +0900

    fix bug

--- cmd/iprange/main.go
+++ cmd/iprange/main.go
@@ -12,7 +12,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "usage of %s: [QUERY]\n", os.Args[0])
 		os.Exit(1)
 	}
-	rngs, err := iputil.Ranges(os.Args[0])
+	rngs, err := iputil.Ranges(os.Args[1])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s: %v\n", err)
 		os.Exit(1)
