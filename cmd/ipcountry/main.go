e6a450d0a61b7bdac712b460ab6aa046afe4c316
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-02-15 18:42:29 +0900

    add ipcountry command

---
+++ cmd/ipcountry/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mattn/iputil"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage of %s: [QUERY]\n", os.Args[0])
+		os.Exit(1)
+	}
+	country, err := iputil.Country(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(country)
+}
