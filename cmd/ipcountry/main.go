1373f2c83511c718f456e1005423df407883f5dc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-02-15 18:53:32 +0900

    fix Fprintf assign

--- cmd/ipcountry/main.go
+++ cmd/ipcountry/main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 	country, err := iputil.Country(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(1)
 	}
 	fmt.Println(country)
--- cmd/iprange/main.go
+++ cmd/iprange/main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 	rngs, err := iputil.Ranges(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", err)
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 		os.Exit(1)
 	}
 	for _, r := range rngs {
